e309e204bd2d0b37be1fb2e7612efb3634cf1cf8
Himanshu Chaudhary <45047527+silent18knight@users.noreply.github.com>
2020-10-16 23:50:46 +0530

    Add files via upload

--- src/test/java/com/philips/CrudSpringDataJpaWithSqLiteApplicationTests.java
+++ src/test/java/com/philips/CrudSpringDataJpaWithSqLiteApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CrudSpringDataJpaWithSqLiteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
